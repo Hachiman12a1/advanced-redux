ea7f02bf036051d8a49f1ce9bb1d9f3454f6b2d1
Dao Van Dat <daovandat2k15@gmail.com>
2022-08-21 10:26:19 +0700

    253. Refresher / Practice: Part 2/2

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import cartSlice from "./cartSlice";
 import uiSlice from "./uiSlice";
 
-
 const store = configureStore({
-    reducer : {ui : uiSlice.reducer}
+  reducer: { ui: uiSlice.reducer, cart: cartSlice.reducer },
 });
 
 export default store;
